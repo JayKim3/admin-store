5f0aefea43580b7aee1093d3ea130604036e0d3c
ksj <lueky4300@naver.com>
2020-11-14 23:09:13 +0900

    first commit

---
+++ src/main/java/com/study/adminstore/AdminStoreApplication.java
@@ -0,0 +1,13 @@
+package com.study.adminstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/adminstore/AdminStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.adminstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
