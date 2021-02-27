3315508026ef066fe8a97fa6383ffb140328c0fc
ksj <lueky4300@naver.com>
2021-02-27 19:14:27 +0900

    setting css & js working...

--- src/main/resources/static/js/setting.js
+++ src/main/resources/static/js/setting.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    $('input[name=input-notification]').change(function(e){
+    const weather = $(".setting__weather");
+
+    const API_KEY = '504117424e458b4722a152467f38f333';
+    const COORDS = 'coords';
+
+    loadCoords();
+
+    $('input[name=input-notification], input[name=input-privacy]').change(function(e){
         const toggleText = $(this).siblings(".slider-text");
         if($(this).is(":checked")) {
             toggleText.text('On');
@@ -8,5 +15,79 @@ $(document).ready(function() {
             toggleText.text('Off');
             toggleText.animate({left: '32px'}, 600);
         }
+    });
+
+    $('input[name=input-mode]').change(function(e){
+        const toggleText = $(this).siblings(".slider-text");
+        if($(this).is(":checked")) {
+            toggleText.text('On');
+            $('html').attr('color-theme', 'dark');
+            toggleText.animate({left: '6px'}, 600);
+        } else {
+            toggleText.text('Off');
+            $('html').attr('color-theme', 'light');
+            toggleText.animate({left: '32px'}, 600);
+        }
     })
-})
+
+    $('input[name=input-language]').change(function (e){
+        const toggleText = $(this).siblings(".slider-text");
+        if($(this).is(":checked")) {
+            toggleText.text('Ko');
+            toggleText.animate({left: '6px'}, 600);
+        } else {
+            toggleText.text('En');
+            toggleText.animate({left: '32px'}, 600);
+        }
+    })
+
+    $('.help-btn').on('click', function () {
+        alert("서비스 준비중입니다.");
+    })
+
+    function getWeather(lat, lng) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+            .then(function(response){
+                return response.json();
+            }).then(function(json){
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.text(`Temp : ${temperature} @ Place : ${place}`);
+        });
+    }
+
+    function saveCoords(coordsObj) {
+        localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+    }
+
+    function handleGeoSuccess(position) {
+        const latitude =  position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const coordsObj =  {
+            latitude : latitude,
+            longitude : longitude
+        };
+        saveCoords(coordsObj);
+        getWeather(latitude, longitude);
+    }
+
+    function handleGeoError() {
+        console.log("Cant access geo location");
+    }
+
+    function askForCoords() {
+        navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
+    }
+
+    function loadCoords() {
+        const loadedCords = localStorage.getItem(COORDS);
+
+        if(loadedCords === null) {
+            askForCoords();
+        } else {
+            const parseCoords = JSON.parse(loadedCords);
+            getWeather(parseCoords.latitude, parseCoords.longitude);
+        }
+    }
+});
